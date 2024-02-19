5b2f0984c73a348b0856c398306a91abdefb6abd
andrecelliers <andrecelliers3@gmail.com>
2024-02-19 17:55:54 -0400

    Small changes

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/website', express.static(path.join(__dirname,  'public')));
 
-app.use('/website', express.static(path.join(__dirname,  'public')));
+app.use(express.static(path.join(__dirname,  'public')));
 
 app.use((req, res) => {
     res.status(404);
