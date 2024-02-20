23ff03c8485f170235bddb48e7d00a1d6ee7e1aa
andrecelliers <andrecelliers3@gmail.com>
2024-02-20 08:40:49 -0400

    small change

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use('/', express.static(path.join(__dirname,  'public')));
+
 app.use('/website', express.static(path.join(__dirname,  'public')));
 
 app.use(express.static(path.join(__dirname,  'public')));
